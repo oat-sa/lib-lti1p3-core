633fc98ed2b364491fb56de736f56dfddfdb55a8
Jonathan VUILLEMIN <ekkinox@gmail.com>
2020-03-04 10:08:44 +0100

    fixed interface

--- src/Security/Key/KeyChainRepositoryInterface.php
+++ src/Security/Key/KeyChainRepositoryInterface.php
@@ -24,7 +24,7 @@ namespace OAT\Library\Lti1p3Core\Security\Key;
 
 interface KeyChainRepositoryInterface
 {
-    public function find(string $id): KeyChainInterface;
+    public function find(string $id): ?KeyChainInterface;
 
     /** @return KeyChainInterface[] */
     public function findBySetName(string $setName): array;
